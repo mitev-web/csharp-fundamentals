cdc99bde05717123e1af76fb2076511eaae0a069
Georgi Mitev <mitev.web@gmail.com>
2012-01-05 12:26:14 +0200

    ref

--- Homeworks/09.Arrays/Task19/Program.cs
+++ Homeworks/09.Arrays/Task19/Program.cs
@@ -7,12 +7,47 @@ namespace Task19
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+     
     //        * Write a program that reads a number N and generates and prints 
     //            all the permutations of the numbers [1 … N]. Example:
     //n = 3  {1, 2, 3}, {1, 3, 2}, {2, 1, 3}, {2, 3, 1}, {3, 1, 2}, {3, 2, 1}
 
+        public static void Main()
+        {
+            int n = 4;
+            int[] used = new int[n];
+            int[] numbers = new int[n];
+            GeneratePermutation(0, n, used, numbers);
+
+        }
+
+        public static void GeneratePermutation(int i, int n, int[] used, int[] numbers)
+        {
+            if (i >= n)
+            {
+                PrintNumbers(numbers);
+                return;
+            }
+
+            for (int k = 0; k < n; k++)
+            {
+                if (used[k] == 0)
+                {
+                    used[k] = 1;
+                    numbers[i] = k + 1;
+                    GeneratePermutation((i + 1), n, used, numbers);
+                    used[k] = 0;
+                }
+            }
+        }
+
+        public static void PrintNumbers(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write("{0} ", numbers[i]);
+            }
+            Console.WriteLine();
         }
     }
 }
--- Homeworks/09.Arrays/Task21/Program.cs
+++ Homeworks/09.Arrays/Task21/Program.cs
@@ -7,13 +7,51 @@ namespace Task21
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+   
     //        Write a program that reads two numbers N and K and generates 
     //        all the combinations of K distinct elements from the set [1..N]. 
     //        Example:
     //N = 5, K = 2  {1, 2}, {1, 3}, {1, 4}, {1, 5}, {2, 3}, {2, 4}, {2, 5}, {3, 4}, {3, 5}, {4, 5}
 
+                    public static void Main()
+        {
+            int n = 5;
+            int k = 2;
+          
+            int[] numbers = new int[n];            
+            GenerateCombination(1, 0, n, k, numbers);
         }
+
+
+        public static void GenerateCombination(int i, int after, 
+            int n, int k, int[] numbers)
+        {
+            if (i > k)
+            {
+                return;
+            }
+
+            for (int j = after + 1; j <= n; j++)
+            {
+                numbers[i - 1] = j;
+                if (i == k)
+                {
+                    PrintNumbers(numbers, i);
+                }
+
+                GenerateCombination((i + 1), j, n, k, numbers);
+            }
+        }
+
+        public static void PrintNumbers(int[] numbers, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("{0} ", numbers[i]);
+            }
+            Console.WriteLine();
+        }
+
+ 
     }
 }
