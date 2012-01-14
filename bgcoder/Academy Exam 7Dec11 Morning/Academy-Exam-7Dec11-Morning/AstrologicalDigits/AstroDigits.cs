d0f386d6cb24407b8a21958f306cf7f3231e680b
Georgi Mitev <mitev.web@gmail.com>
2012-01-15 01:39:35 +0200

    ref

---
+++ bgcoder/Academy Exam 7Dec11 Morning/Academy-Exam-7Dec11-Morning/AstrologicalDigits/AstroDigits.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class AstrologicalDigits
+    {
+        static void Main(string[] args)
+        {
+            string N = Console.ReadLine();
+            List<int> numbers = new List<int>();
+
+            int number = 0;
+            int sum = 10;
+
+            while (sum > 9)
+            {
+                sum = 0;
+                numbers.Clear();
+                foreach (char c in N)
+                {
+                    int.TryParse(c.ToString(), out number);
+                    numbers.Add(number);
+                }
+                sum = numbers.Sum();
+                N = sum.ToString();
+            }
+            Console.WriteLine(sum);
+        }
+    }
+
---
+++ bgcoder/BNOI 2012 - Round 1 - Group C (7-8 grade)/BNOI-2012-GroupC/Balls/Balls.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+//Given are n boxes, which were empty. During the day Ivancho placed balls k times in some of the boxes. Write program balls, which computes the number of empty boxes at the end of the day.
+
+//The program reads data by the standard input. On the first line are given the values of n and k. On each of the next k lines a box number (1,2,…n), in which Ivancho has set balls, is given.
+
+//On the standard output the program has to write an integer, which is the sought value.
+
+//Constraints:
+//1 < n < 1 000 000 000 000
+//1 < k < 101
+
+//Example.
+//Input:
+//5 4
+//1 
+//1 
+//2 
+//2 
+
+class Balls
+{
+    static void Main(string[] args)
+    {
+        decimal[] numbers = Console.ReadLine().Split(' ').Select(x => decimal.Parse(x)).ToArray();
+
+        //boxes
+        decimal N = numbers[0];
+
+        int K = int.Parse(numbers[1].ToString());
+
+        decimal[] filledboxes = new decimal[K];
+
+        for (int i = 0; i < K; i++)
+        {
+            filledboxes[i] = decimal.Parse(Console.ReadLine());
+        }
+
+        decimal differenFilledBoxes = filledboxes.Distinct().ToArray().LongCount();
+
+        Console.WriteLine(N-differenFilledBoxes);
+    }
+}
+
