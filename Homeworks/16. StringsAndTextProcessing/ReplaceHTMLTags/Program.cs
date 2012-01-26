b62dfbe57584c20fef31fa9f2690e7ad4d5b2883
Georgi Mitev <mitev.web@gmail.com>
2012-01-26 21:13:50 +0200

    red

--- Homeworks/16. StringsAndTextProcessing/ReplaceHTMLTags/Program.cs
+++ Homeworks/16. StringsAndTextProcessing/ReplaceHTMLTags/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ReplaceHTMLTags
 {
@@ -10,17 +9,37 @@ namespace ReplaceHTMLTags
         //Write a program that replaces in a HTML document
         //given as string all the tags <a href="…">…</a>
         //with corresponding tags [URL=…]…/URL]. Sample HTML fragment:
-
         //<p>Please visit <a href="http://academy.telerik. com">our site</a>
+
         //to choose a training course. Also visit <a href="www.devbg.org">
         //our forum</a> to discuss the courses.</p>
 
         //<p>Please visit [URL=http://academy.telerik. com]our site[/URL] to 
         //choose a training course. Also visit [URL=www.devbg.org]our forum[/URL] 
         //to discuss the courses.</p>
-
         static void Main(string[] args)
         {
+            string text = "Also visit <a href=www.devbg.org>" +
+                          " dd orum</a> to discuss the courses.</p> Also visit <a href=www.devbg.org>" +
+                          " our forum</a> to discuss the courses.</p>";
+
+
+            //string regex = @"((www\.|(http|https|ftp|news|file)+\:\/\/)[&#95;.a-z0-9-]+\.[a-z0-9\/&#95;:@=.+?,##%&~-]*[^.|\'|\# |!|\(|?|,| |>|<|;|\)])";
+            //Regex r = new Regex(regex, RegexOptions.IgnoreCase);
+            //return r.Replace(msg, "<a href=\"$1\" title=\"Click to open in a new window or tab\" target=\"&#95;blank\">$1</a>").Replace("href=\"www", "href=\"http://www");
+
+            string pattern = @"<a href=(.*)>(.*)</a>";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(text);
+
+
+
+            text = Regex.Replace(text, pattern, "[URL=$1]$2[/URL]");
+
+            Console.WriteLine(text);
+           
         }
     }
 }
