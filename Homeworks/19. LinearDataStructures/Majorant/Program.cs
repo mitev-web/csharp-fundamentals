4a1f945b9c7511375960f46e42ce0863171fe2db
Georgi Mitev <mitev.web@gmail.com>
2012-01-29 22:39:07 +0200

    ref

--- Homeworks/19. LinearDataStructures/Majorant/Program.cs
+++ Homeworks/19. LinearDataStructures/Majorant/Program.cs
@@ -12,9 +12,40 @@ namespace Majorant
         //    Write a program to find the majorant of given array (if exists). 
         //Example:
         //{2, 2, 3, 3, 2, 3, 4, 3, 3}  3
+        public static List<int> allNumbers = new List<int> { 2, 2, 3, 3, 2, 3, 4, 3, 3 };
 
         static void Main(string[] args)
         {
+            
+
+
+
+        }
+
+
+
+        public static KeyValuePair<bool,int> ContainedTimes(int number, List<int> numbers, int times = 0)
+        {
+            List<int> tempNumbers = new List<int>();
+            tempNumbers.AddRange(numbers);
+
+            if (tempNumbers.Contains(number))
+            {
+                tempNumbers.Remove(number);
+                return ContainedTimes(number, tempNumbers, ++times);
+            }
+            else
+            {
+                if (times > allNumbers.Count() / 2)
+                {
+                    new KeyValuePair<bool,int>()
+                    return ;
+                }
+                else
+                {
+                    ret
+                }
+            }
         }
     }
 }
