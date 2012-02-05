187e0078be2ec98211bf7f5422310fe9f6986728
Georgi Mitev <mitev.web@gmail.com>
2012-02-05 21:29:59 +0200

    ref

---
+++ bgcoder/Academy Preliminary Feb12/Acad-Preliminary-Feb12/AirplaneDrinks/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Linq;
+
+namespace GenomeDecoder
+{
+    class Program
+    {
+        static int N;
+        static bool[] pass = null;
+        static bool[] isServed = null;
+
+        static int seconds = 48;
+        static int drinks = 7;
+        static bool currentlyServing;
+        static bool nextDrink = false;
+        static bool passangersAreServed = false;
+
+        static void Main(string[] args)
+        {
+            //true = tea
+            N = int.Parse(Console.ReadLine());
+            int K = int.Parse(Console.ReadLine());
+            pass = new bool[N];
+            isServed = new bool[N];
+
+            for (int i = 0; i < K; i++)
+            {
+                pass[int.Parse(Console.ReadLine()) - 1] = true;
+            }
+
+            currentlyServing = true;
+            CalculateServingTime();
+            int secondsWhenStartingWithTea = seconds;
+
+            ResetParams();
+            currentlyServing = false;
+            CalculateServingTime();
+            int secondsWhenStartingWithCoffee = seconds;
+
+            if (secondsWhenStartingWithCoffee < secondsWhenStartingWithTea)
+            {
+                Console.WriteLine(secondsWhenStartingWithCoffee);
+            }
+            else
+            {
+                Console.WriteLine(secondsWhenStartingWithTea);
+            }
+        }
+
+        private static void CalculateServingTime()
+        {
+
+            while (!passangersAreServed)
+            {
+                ServeFromBeginning();
+
+
+
+                //ServeFromEnd();
+           
+
+            }
+        }
+  
+        private static void ServeFromBeginning()
+        {
+            for (int i = 0; i < N; i++, seconds++)
+            {
+                if (RefillIsNeeded())
+                {
+                    Refill(i);
+                    i = 0;
+                }
+                if (isServed[i] == false)
+                {
+                    if (pass[i] == currentlyServing)
+                    {
+                        isServed[i] = true;
+                        drinks--;
+                        seconds += 4;
+                        if (PassangersAreServed())
+                        {
+                            passangersAreServed = true;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+  
+        private static void ServeFromEnd()
+        {
+            for (int i = N - 1; i >= 0; i--,seconds++)
+            {
+                if (RefillIsNeeded())
+                {
+                    Refill(i);
+                    return;
+                }
+                if (isServed[i] == false)
+                {
+                    if (pass[i] == currentlyServing)
+                    {
+                        isServed[i] = true;
+                        seconds += 4;
+                        drinks--;
+                        if (PassangersAreServed())
+                        {
+                            passangersAreServed = true;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static bool RefillIsNeeded()
+        {
+            bool refillIsneeded = false;
+
+            for (int i = 0; i < N; i++)
+            {
+                if (pass[i] != currentlyServing && !isServed[i])
+                {
+                    nextDrink = pass[i];
+                }
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                if (pass[i] != currentlyServing && !isServed[i] && drinks == 0)
+                {
+                    refillIsneeded = true;
+                    nextDrink = pass[i];
+                    return true;
+                }
+
+                refillIsneeded = true;
+                if (pass[i] == currentlyServing && !isServed[i] && drinks > 0)
+                {
+                    return false;
+                }
+            }
+
+            return refillIsneeded;
+        }
+  
+        private static void Refill(int position)
+        {
+            seconds += position + 2 + 47;
+            currentlyServing = nextDrink;
+       
+            drinks = 7;
+        }
+  
+        private static bool PassangersAreServed()
+        {
+            bool areServerd = true;
+            for (int i = 0; i < isServed.Length; i++)
+            {
+                if (isServed[i] == false)
+                {
+                    areServerd = false;
+                }
+            }
+            return areServerd;
+        }
+
+        private static void ResetParams()
+        {
+            pass = new bool[N];
+            isServed = new bool[N];
+            pass[1] = true;
+            pass[2] = true;
+            seconds = 48;
+            drinks = 7;
+            nextDrink = false;
+            passangersAreServed = false;
+        }
+    }
+}
---
+++ bgcoder/Academy Preliminary Feb12/Acad-Preliminary-Feb12/TicTacToe/Program.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static int[,] board = new int[3, 3]
+        {
+            { 0, 0, 3, },
+            { 1, 3, 1, },
+            { 3, 3, 3, }
+        };
+        static int xComb = 0;
+        static int evenComb = 0;
+        static int zeroComb = 0;
+   
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                CheckForCombinations(0, 0,1);
+            }
+            catch (Exception)
+            {
+            }
+            
+            
+            Console.WriteLine(xComb);
+            Console.WriteLine(evenComb);
+            Console.WriteLine(zeroComb);
+        }
+  
+        private static void CheckForCombinations(int row, int col, int startsWith)
+        {
+             
+            if (CountForCombinations(0) > 0)
+                zeroComb += CountForCombinations(0);
+
+            if (CountForCombinations(1) > 0)
+                xComb += CountForCombinations(0);
+ 
+        
+            if (row > 2)
+            {
+              
+                if (CountForCombinations(0) == 0 && CountForCombinations(1) == 0)
+                {
+                    evenComb++;
+                }
+                // throw new Exception("Solution found");
+            }
+            else
+            {
+                // Skip cells which are not empty
+                while (board[row, col] != 3)
+                {
+                    if (++col > 2)
+                    {
+                        col = 0;
+                        row++;
+
+                        // Throw an exception to stop the process if the puzzle is solved
+                        if (row > 2)
+                        {
+                            if (CountForCombinations(0) == 0 && CountForCombinations(1) == 0)
+                            {
+                                evenComb++;
+                            }
+                            throw new Exception("Solution found");
+                        }
+                    }
+                }
+
+                for (int num = 0; num < 2; num++)
+                {
+                    board[row, col] = startsWith;
+                   
+                    startsWith = ++startsWith % 2;
+
+                    // Delegate work on the next cell to a recursive call
+                    if (col < 2)
+                        CheckForCombinations(row, col + 1,startsWith);
+                    else
+                        CheckForCombinations(row + 1, 0, startsWith);
+                }
+ 
+
+                // No valid number was found, clean up and return to caller
+                board[row, col] = 3;
+            }
+        }
+  
+        private static int CountForCombinations(int digit)
+        {
+            int combinations = 0;
+            for (int i = 0; i < 3; i++)
+            { //horizontal
+                if ((board[i, 0] == board[i, 1]) && (board[i, 1] == board[i, 2]) && (board[i, 2] == digit))
+                {
+                    combinations++;
+                }
+                //vertical
+                if ((board[0, i] == board[1, i]) && (board[1, i] == board[2, i]) && (board[2, i] == digit))
+                {
+                    combinations++;
+                }
+            }
+            //diagonal
+            if ((board[0, 0] == board[1, 1]) && (board[1, 1] == board[2, 2]) && (board[2, 2] == digit))
+            {
+                combinations++;
+            }
+            //diagonal
+            if ((board[2, 0] == board[1, 1]) && (board[1, 1] == board[0, 2]) && (board[0, 2] == digit))
+            {
+                combinations++;
+            }
+            return combinations;
+        }
+
+        static bool BoardIsValid()
+        {
+            int zcombinations = 0;
+            int xcombinations = 0;
+            for (int i = 0; i < 3; i++)
+            { //horizontal
+                if ((board[i, 0] == board[i, 1]) && (board[i, 1] == board[i, 2]) && (board[i, 2] == 0))
+                {
+                    zcombinations++;
+                }
+                //vertical
+                if ((board[0, i] == board[1, i]) && (board[1, i] == board[2, i]) && (board[2, i] == 0))
+                {
+                    zcombinations++;
+                }
+            }
+            //diagonal
+            if ((board[0, 0] == board[1, 1]) && (board[1, 1] == board[2, 2]) && (board[2, 2] == 0))
+            {
+                zcombinations++;
+            }
+            //diagonal
+            if ((board[2, 0] == board[1, 1]) && (board[1, 1] == board[0, 2]) && (board[0, 2] == 0))
+            {
+                zcombinations++;
+            }
+            for (int i = 0; i < 3; i++)
+            { //horizontal
+                if ((board[i, 0] == board[i, 1]) && (board[i, 1] == board[i, 2]) && (board[i, 2] == 1))
+                {
+                    xcombinations++;
+                }
+                //vertical
+                if ((board[0, i] == board[1, i]) && (board[1, i] == board[2, i]) && (board[2, i] == 1))
+                {
+                    xcombinations++;
+                }
+            }
+            //diagonal
+            if ((board[0, 0] == board[1, 1]) && (board[1, 1] == board[2, 2]) && (board[2, 2] == 1))
+            {
+                xcombinations++;
+            }
+            //diagonal
+            if ((board[2, 0] == board[1, 1]) && (board[1, 1] == board[0, 2]) && (board[0, 2] == 1))
+            {
+                xcombinations++;
+            }
+
+            //if (CountDigit(0) + 1 == CountDigit(1) || CountDigit(0) == CountDigit(1))
+            //{
+                if (xcombinations > 0 && zcombinations > 0)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+               // return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+        }
+
+        public static void Print()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(board[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static int CountDigit(int digit)
+        {
+            int count = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == digit)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
