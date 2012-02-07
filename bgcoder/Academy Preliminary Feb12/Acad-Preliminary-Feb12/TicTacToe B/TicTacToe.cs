a327aab32af5d48496eb133df1c832b9bae4aeac
Georgi Mitev <mitev.web@gmail.com>
2012-02-07 23:01:45 +0200

    ref

--- bgcoder/Academy Preliminary Feb12/Acad-Preliminary-Feb12/TicTacToe B/TicTacToe.cs
+++ bgcoder/Academy Preliminary Feb12/Acad-Preliminary-Feb12/TicTacToe B/TicTacToe.cs
@@ -3,17 +3,15 @@ using System.Linq;
 
 class TicTacToe
 {
-
     /// <summary>
     /// code uploaded at:
-    /// http://ideone.com/4M1Se
+    /// 
     /// </summary>
-
     static int[,] board = new int[3, 3]
     {
         { 0, 0, 3, },
-        { 1, 3, 1, },
-        { 3, 3, 3, }
+        { 1, 1, 0, },
+        { 1, 1, 0, }
     };
 
     //1 is X
@@ -31,67 +29,65 @@ class TicTacToe
         Console.WriteLine(xWins);
         Console.WriteLine(draw);
         Console.WriteLine(zWins);
-        
     }
 
-    static void FindNumberOfWins(int row, int col, int nextDigit = 0)
+    static void FindNumberOfWins(int row, int col)
     {
-        if (IsOutsideOfBoard(row, col))
-            return;
-
-        //we scroll until we find an empty cell
-        while (board[row, col] != 3)
-        {
-            if (++col > 2)
-            {
-                col = 0;
-                row++;
-                if (row > 2)
-                {
-                    //outside of board
-                    return;
-                }
-            }
-        }
-
         //if X and 0 are even - X will be on the move, because he started first
+        int nextDigit = 1;
         if (CountDigit(0) == CountDigit(1))
         {
-            nextDigit = 1;
+           nextDigit = 1;
         }
         else
         {
             nextDigit = 0;
         }
 
-        board[row, col] = nextDigit;
 
         if (CheckForWin(1))
         {
             xWins++;
+            return;
         }
 
         if (CheckForWin(0))
         {
             zWins++;
-      
+            return;
         }
 
         if (CountDigit(3) == 0 && !CheckForWin(1) && !CheckForWin(0))
         {
             draw++;
+            return;
         }
 
-        FindNumberOfWins(row, col - 1, nextDigit); // left
-        FindNumberOfWins(row + 1, col, nextDigit); // down
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (board[i, j] == 3)
+                {
+                    //cell is empty - so try recursion
 
-        FindNumberOfWins(row, col + 1, nextDigit); // left
-        FindNumberOfWins(row - 1, col, nextDigit); // up
-
-        // Mark back the current cell as free
+                    board[i, j] = nextDigit;
+                    FindNumberOfWins(i, j);
+                    //board[i, j] = 0;
+                    //FindNumberOfWins(i, j);
+                    //board[i, j] = 1;
+                    //FindNumberOfWins(i, j);
+             
+                    
+                }
+            }
+        }
         board[row, col] = 3;
+
     }
-  
+
+    // Mark back the current cell as free
+
     private static bool IsOutsideOfBoard(int row, int col)
     {
         if (row > 2 || col > 2 || row < 0 || col < 0)
